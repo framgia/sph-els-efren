6dd4c85fc0e9941b38f064f74d49d2d444a32823
unknown <catedrilla53@gmail.com>
2022-08-02 16:52:25 +0800

    updating css style

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,12 +4,15 @@ const Content = () => {
 
     return(
        <div className="ui container main-content">
-                <div className="ui breadcrumb">
-                    <a className="section">Home</a>
+                <div className="ui huge breadcrumb">
+                    <a className="section">  Home</a>
                     <div className="divider"> / </div>
                     <a className="section">Store</a>
                     <div className="divider"> / </div>
-                    <div className="active section">T-Shirt</div>
+                    <div class="ui medium basic circular label" id="public-badge">
+                    Public
+                    </div>
+
                 </div>
 
                 <div className="ui top attached tabular menu">
@@ -23,8 +26,32 @@ const Content = () => {
                 <div className=" item">Insights</div>
                 </div>
                 <div className="ui bottom attached active tab segment">
-                <p></p>
-                <p></p>
+               <table className="ui single line table">
+                            <thead>
+                                <tr>
+                                    <th colSpan="3">
+                                   342 Open
+                                    </th>
+                                
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <td className="collapsing">
+                                    <i className="folder icon"></i> node_modules
+                                </td>
+                                <td>Initial commit</td>
+                                <td className="right aligned collapsing">10 hours ago</td>
+                                </tr>
+                                <tr>
+                                <td>
+                                    <i className="folder icon"></i> test
+                                </td>
+                                <td>Initial commit</td>
+                                <td className="right aligned">10 hours ago</td>
+                                </tr>
+                            </tbody>
+                            </table>
                 </div>
        
        </div>

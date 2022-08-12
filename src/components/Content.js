1017eca684985c453fa4583016cde41923463319
unknown <efren.catedrilla@sun-asterisk.com>
2022-08-12 13:31:39 +0800

    updating author data

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,29 +1,22 @@
-<<<<<<< Updated upstream
-import React from "react";
-=======
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Tippy from "@tippyjs/react";
 import 'tippy.js/dist/tippy.css';
 import moment from 'moment';
->>>>>>> Stashed changes
 
 const Content = ({issues ,githubState}) => {
 
-<<<<<<< Updated upstream
-=======
     const [author, setAuthor] = useState('')
     const [authors, setAuthors] = useState([])
 
->>>>>>> Stashed changes
     let count=0,count2 = 0, active, closed;
     active = issues.filter(item => item.state === 'open');
     closed = issues.filter(item => item.state === 'closed');
     count = active.length;
     count2 = closed.length;
-<<<<<<< Updated upstream
-=======
+
+
 
     const loadAuthor = async () => {
        if(author != '' && author != 'undefined') {
@@ -52,46 +45,27 @@ const Content = ({issues ,githubState}) => {
     
     const renderedList = issues.map((issue) => {
 
-     const LabelItem =  issue.labels.map((label,index) => {
+        const LabelItem = issue.labels.map((label, index) => {
             return(
-                <span key={index} className="ui basic label" style={{ backgroundColor:`#${label.color}`, opacity:'0.5',borderRadius:'2em',border:`solid grey 0.5px` }}> {label.name}</span>
+                <span key={index} className="ui basic label" style={{ backgroundColor:`#${label.color}`, opacity:'0.5',borderRadius:'2em', border:`solid grey` }}> {label.name}</span>
             );
          })
->>>>>>> Stashed changes
 
-    const renderedList = issues.map((issue, index) => {
         return(
             <tr key={issue.id}>
-                <td className="collapsing">
-                    <i className="dot circle outline large green icon"> </i>
-<<<<<<< Updated upstream
-                    {issue.title}
-                </td>
-                <td>{issue.user.login}</td>
-                <td>{issue.state}</td>
-                <td className="right aligned collapsing">{issue.created_at}</td>
-=======
+                <td>
+                    <i className="dot circle outline large green icon" />
                     <Link to={`/page_details/${issue.number}`} style={{ color:'white' }} > <span style={{ fontSize:'16px',fontWeight:'bold' }}>{issue.title}</span></Link> {LabelItem}<br/>
                     <span style={{ marginLeft:'30px', fontSize:'12px',color:'#8b949e' }}>{`#${issue.number}`} {issue.state === 'open' ? 'opened': issue.state} {moment.utc(issue.created_at).local().startOf('seconds').fromNow()} by </span>
                     <Tippy  content={AuthorData()} >
                         <span style={{ fontSize:'12px',color:'#8b949e' }} onMouseOver={() => setAuthor(issue.user.login)} >{issue.user.login}  </span>
                     </Tippy>
                 </td>
->>>>>>> Stashed changes
             </tr>
         ); 
     });
  
     return( 
-<<<<<<< Updated upstream
-        <table className="ui celled striped table inverted">
-            <thead>
-                <tr>
-                    <th colSpan="3">
-                    <i className="dot circle outline large white icon"></i>
-                    {count} Open <i className="check small white icon"></i> {count2} Closed
-                    <div className="ui right aligned"> test</div>
-=======
         <table className="ui small single line table inverted">
             <thead>
                 <tr>
@@ -102,7 +76,6 @@ const Content = ({issues ,githubState}) => {
                                 :(githubState.value  === 'closed') ? <div> <span style={{ fontWeight:'lighter'}}>{count} Open</span> <span>  {count2} Closed </span> <i className="check small white icon" /> </div>
                                     : <div> <span>{count} Open</span> <span> {count2} Closed</span></div>
                         }
->>>>>>> Stashed changes
                     </th>
                 </tr>
             </thead>
